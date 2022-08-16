4b9bf918e13a463c61801681778af5f028110fd6
Alec Delaney <tekktik@gmail.com>
2022-08-16 18:09:16 -0400

    Update version string

--- adafruit_ticks.py
+++ adafruit_ticks.py
@@ -24,7 +24,7 @@ Implementation Notes
 # imports
 from micropython import const
 
-__version__ = "0.0.0-auto.0"
+__version__ = "0.0.0+auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_ticks.git"
 
 _TICKS_PERIOD = const(1 << 29)
